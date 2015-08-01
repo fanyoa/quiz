9253e24dc273e0178e644e9e4771077530ecb1f9
fanyoa <fanyoa@gmail.com>
2015-08-01 17:53:15 +0200

    Base de datos inicializada

---
+++ models/models.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+
+var sequelize = new Sequelize(null,null,null,{dialect:'sqlite',storage:'quiz.sqlite'});
+
+// Importar definicion de la tabla Quiz
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// exportar tablas
+exports.Quiz = Quiz; 
+
+sequelize.sync().success(function() {
+    Quiz.count().success(function (count){
+          if(count === 0) {   // la tabla se inicializa solo si está vacía
+            Quiz.create( 
+              [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+                {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+              ]
+            ).success(function(){console.log('Base de datos inicializada')});
+          };
+    });
+});
+
+
