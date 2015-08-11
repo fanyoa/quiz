f30d598dbd6fb294e374989a3a742b5b0c12defe
fanyoa <fanyoa@gmail.com>
2015-08-11 16:21:09 +0200

    Autorización

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,10 +2,13 @@ var users = {admin:{id:1,username:"admin",password:"1234"},
 			pepe:{id:2,username:"pepe",password:"5678"}
 };
 
-export.autenticar = function(login,password,callback){
-	if(users[login]){
+
+exports.autenticar = function(login, password, callback) {
+	if (users[login]) {
 		if (password===users[login].password){
-			callback(null,users[login]);
-		}else{callback(new Error('Password erroneo'));}
-	}else{ callback(new Error('No existe el usuario'));}
-}
+			callback(null, users[login]);
+		}
+		else { callback(new Error('Password erroneo.')); } 	
+	} else { callback(new Error('No existe el usuario'))}
+};
+
